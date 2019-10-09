f54d82b9c144882a7a5b0e1fc97deabc35514dfc
hjhsu <hjhsu@gmail.com>
2019-10-09 10:51:31 +0800

    新增 Header Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
-
+import Header from './component/Header'
+import Footer from './component/Footer'
 class App extends Component{
   render(){
     return(
       <div className="App">
+        <Header></Header> 
         <h1> Hello, World!!</h1>
+        <Footer></Footer>  
       </div>
     );
   }
